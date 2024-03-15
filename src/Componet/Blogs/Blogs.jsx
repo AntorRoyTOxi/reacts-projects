0d7b71b9174c7d4bf083623ad4464e1c2c5e8056
AntorRoyTOxi <antorroyanu558@gmail.com>
2024-03-15 22:37:51 +0600

    ok

---
+++ src/Componet/Blogs/Blogs.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blogs = () => { 
+     const [blogs,setBlogs]=useState([]); 
+      useEffect(()=>{
+ 
+         fetch('blogs.json') 
+          .then(res=>res.json())
+           .then(data=>setBlogs(data))
+         
+      },[])
+    return (
+        <div>
+            <h1> this is antor kumar roy</h1>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/Componet/Hearder/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div> 
+           <h1 className='text-6xl font-bold bg-red-400 p-10'>Knowledge </h1>
+      
+            
+        </div>
+    );
+};
+
+export default Header;
